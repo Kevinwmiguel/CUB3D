1b42d0d11f81eb3d393e82ed1cf3ec869c9a1aab
made-jes <chivas.maria17@hotmail.com>
2026-03-17 23:38:01 +0000

    adding the logic using pixels

--- srcs/draws4.c
+++ srcs/draws4.c
@@ -6,7 +6,7 @@
 /*   By: made-jes <made-jes@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/24 21:26:33 by made-jes          #+#    #+#             */
-/*   Updated: 2026/02/24 21:28:14 by made-jes         ###   ########.fr       */
+/*   Updated: 2026/03/13 23:29:28 by made-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 static void	hit_image(t_cub3d *g, float *pos, float step_x, float step_y)
 {
-	if (fabs(step_x) > fabs(step_y))
+	if (g->hit.side == 0)
 	{
 		if (step_x > 0)
 			g->hit.tex_id = 3;
@@ -32,6 +32,32 @@ static void	hit_image(t_cub3d *g, float *pos, float step_x, float step_y)
 	}
 }
 
+static int	get_side(t_cub3d *g, float angle, float px, float py)
+{
+	float	rx;
+	float	ry;
+	int		sx;
+	int		sy;
+
+	rx = px;
+	ry = py;
+	sx = 0;
+	sy = 0;
+	while (!touch_wall(rx, py, g))
+	{
+		rx += cos(angle);
+		sx++;
+	}
+	while (!touch_wall(px, ry, g))
+	{
+		ry += sin(angle);
+		sy++;
+	}
+	if (sx <= sy)
+		return (0);
+	return (1);
+}
+
 void	trace_ray(t_cub3d *g, float angle, float *pos)
 {
 	float	step_x;
@@ -40,6 +66,7 @@ void	trace_ray(t_cub3d *g, float angle, float *pos)
 
 	step_x = cos(angle);
 	step_y = sin(angle);
+	g->hit.side = get_side(g, angle, pos[0], pos[1]);
 	safety = 0;
 	while (!touch_wall(pos[0], pos[1], g))
 	{
@@ -50,3 +77,4 @@ void	trace_ray(t_cub3d *g, float angle, float *pos)
 	}
 	hit_image(g, pos, step_x, step_y);
 }
+	
